5ab9fa0405cc5a99e114080decba8cbe9d5f95a5
khzhu <kzhu@uhnresearch>
2018-04-22 13:28:32 -0400

    add tomcat support

--- src/main/java/com/pughlab/trialmatchapi/TrialMatchApiApplication.java
+++ src/main/java/com/pughlab/trialmatchapi/TrialMatchApiApplication.java
@@ -2,11 +2,27 @@ package com.pughlab.trialmatchapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
+public class TrialMatchApiApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(TrialMatchApiApplication.class);
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(TrialMatchApiApplication.class, args);
+    }
+
+}
+
+/*@SpringBootApplication
 public class TrialMatchApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TrialMatchApiApplication.class, args);
     }
-}
+}*/
